6af5bf9af50ce4bd1c3fa5ad41cb4bf1283842a0
Mirinano <43010381+Mirinano@users.noreply.github.com>
2018-09-06 21:10:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ client = discord.Client()
 
 master_cmd_id = "" #ここに管理者連絡室内の横断BAN実行専用チャンネルのID
 
+done_number = 5 #実行に必要な⭕の数
+cancel_number = 3 #中止に必要な❌の数
+
 @asyncio.coroutine
 def force_ban(user_id, server_id, delete_message_days=1):
     yield from client.http.ban(user_id, server_id, delete_message_days)
@@ -35,7 +38,7 @@ async def on_message(message):
             content = "次のユーザーをこのBOTを導入している次のサーバーすべてでBANします。\n"
             ban_user_info = "名前: " + ban_user.name + "\nメンション: <@" + ban_user_id + ">\n"
             ban_server = "```" + "\n".join(servers_name_list) + "```\n"
-            action_message = "⭕の数が5つに達すると実行されます。❌の数が3つに達すると拒否されます。\nまた、5分間リアクションの追加がない場合でも拒否されます。"
+            action_message = "⭕の数が" + str(done_number) + "つに達すると実行されます。❌の数が" + str(cancel_number) + "つに達すると拒否されます。\nまた、5分間リアクションの追加がない場合でも拒否されます。"
             check_content = content + ban_user_info + ban_server + action_message
             check_msg = await client.send_message(send_ch, check_content)
             await client.add_reaction(check_msg, "⭕")
@@ -55,14 +58,14 @@ async def on_message(message):
                         result = "err"
                     elif target_reaction.reaction.emoji == "⭕":
                         reaction_count["done"] += 1
-                        if reaction_count["done"] == 5:
+                        if reaction_count["done"] == done_number:
                             loop = None
                             result = "done"
                         else:
                             pass
                     elif target_reaction.reaction.emoji == "❌":
                         reaction_count["cancel"] += 1
-                        if reaction_count["cancel"] == 3:
+                        if reaction_count["cancel"] == cancel_number:
                             loop = None
                             result = "cancel"
                         else:
@@ -87,5 +90,5 @@ async def on_message(message):
                         fail_server.append(servers_dict_list[sil])
                 result_content = "横断BANが完了しました。\n実行できたサーバー\n```\n" + "\n".join(done_server) + "\n```\n失敗したサーバー\n```\n" + "\n".join(fail_server) + "\n```"
                 await client.send_message(send_ch, result_content)
-
+                
 client.run("Token")
